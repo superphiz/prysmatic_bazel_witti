22ee04e35d160566cf49dcdbd62fa96c280c127d
super phiz <superphiz@protonmai.com>
2020-06-21 20:44:13 -0500

    fixed http endpoint

--- beacon_chain_restarter.sh
+++ beacon_chain_restarter.sh
@@ -7,7 +7,7 @@ do
 	cd $HOME/prysm && $HOME/bin/bazel build //beacon-chain:beacon-chain
 	cd $HOME/prysm && unbuffer $HOME/bin/bazel run //beacon-chain -- \
 		--witti-testnet \
-		--http-web3provider=$HOME/.ethereum/goerli/geth.ipc
+                --http-web3provider=http://localhost:8545/ \
 		--datadir=$HOME/prysm \
 		--p2p-host-ip=$(curl -s v4.ident.me) \
 		--p2p-max-peers=100 \
